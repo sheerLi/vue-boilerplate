a3e9c4a0e6617b087653eb26b7537db3482ce713
sheerLi <m17805850283@163.com>
2022-07-23 10:49:54 +0800

    ci(vite.config.ts): update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: '0.0.0.0',
-    port: 3004,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+
+  return {
+    base: env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    plugins: [vue()],
+    server: {
+      host: '0.0.0.0',
+      port: 3004,
+    },
+  }
 })
